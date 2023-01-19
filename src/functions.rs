0cbaae3681f2519ca0ef5c7954ba2e23a3da48f5
AlexRaiteri <alex.raiteri@studenti.unimi.it>
2023-01-19 15:25:58 +0100

    Implemented generic polynomial functions

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,41 @@
 pub trait Function {
     fn evaluate(&self, x : &f32) -> f32;
 }
+// PolynomialFunction : a + bx + cx^2 + dx^3 + ....
+pub struct PolynomialFunction {
+    pub parameters : Vec<f32>,
+}
 
+impl Function for PolynomialFunction {
+    fn evaluate(&self, x: &f32) -> f32 {
+        let mut total : f32 = self.parameters[0] ;
+        for i in 1..self.parameters.len(){
+            let mut contr = self.parameters[i] ;
+            for _n in 1..i+1 {
+                contr = contr * x ;
+            }
+            total += contr ;
+        }
+        total
+    }
+}
+
+
+
+
+pub struct Sin{}
+
+impl Function for Sin{
+    fn evaluate(&self, x : &f32) -> f32 {
+        x.sin()
+    }
+}
+
+
+
+
+
+/*
 pub struct Line{
     pub a : f32,
     pub b : f32,
@@ -40,11 +74,4 @@ impl Function for Cubic{
     }
 }
 
-
-pub struct Sin{}
-
-impl Function for Sin{
-    fn evaluate(&self, x : &f32) -> f32 {
-        x.sin()
-    }
-}
+*/
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use integrals::*;
 
 
 fn main() {
-    let line = Line{ a : 1.1 , b : 2.2 } ;
-    let parabola = Parabola{ a : 3.4 , b : 6.5 , c : 7.8 } ;
-    let cubic = Cubic{ a : 7.6 , b : 1.9 , c : 9.1 , d : 11.2 } ;
+    let line = PolynomialFunction{ parameters : vec![1.1,2.2]} ;
+    let parabola = PolynomialFunction{ parameters : vec![3.4,6.5,7.8]} ;
+    let cubic = PolynomialFunction{ parameters : vec![7.6,1.9,9.1,11.2]} ;
     let sin = Sin{} ;
-    // println!("{}", line1.evaluate(&x)) ;
+
     for i in 1..10 {
         println!("{i} : Line  R =  {}, T = {} , S1 = {} , S2 = {}", rectangular_integral(&line, i * 10 ) ,
                  trapezoidal_integral(&line, i * 10 ), simpson1(&line, i * 10),
