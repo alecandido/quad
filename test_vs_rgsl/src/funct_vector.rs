f504bc13ef8946c39c7b70fcb850c29935aa100f
Alessandro Candido <candido.ale@gmail.com>
2023-06-20 14:06:36 +0200

    Drop rgsl tests

--- test_vs_rgsl/src/funct_vector.rs
+++
@@ -1,9 +0,0 @@
-use crate::*;
-use std::sync::{Arc, Mutex};
-
-
-#[derive(Clone)]
-pub struct FnPa {
-    pub components : Arc<dyn Fn(f64)->f64 + Send + Sync>,
-}
-
--- test_vs_rgsl/src/time.rs
+++
@@ -1,8 +0,0 @@
-use std::time::Instant;
-
-pub fn time <X: std::fmt::Debug>(x : X) -> (){
-    let start = Instant::now();
-    let a = x;
-    println!("{:?}",a);
-    println!("{:?}",start.elapsed());
-}
