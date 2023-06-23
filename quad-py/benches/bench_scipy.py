deed9897d93ec520e48fc129c4504c74dc03461e
Alessandro Candido <candido.ale@gmail.com>
2023-06-23 13:08:17 +0200

    Comment missing item in package

--- quad-py/benches/bench_scipy.py
+++ quad-py/benches/bench_scipy.py
@@ -15,22 +15,20 @@ t1 = 0.0
 t2 = 0.0
 
 for a in var:
-    f = lambda x : (math.cos(x),math.sin(x),math.cos(x),math.sin(x))
+    f = lambda x: (math.cos(x), math.sin(x), math.cos(x), math.sin(x))
     epsabs = 1.0e-3
     epsrel = 0.0
     a = 0.0
     b = 10000.0
 
-
-
-    start = time.time()
-    res_array = quad.qag_array(f,a,b,epsabs,epsrel,6,10000,4)
-    end = time.time()
-    t1 += end - start
-    print(t1)
+    #  start = time.time()
+    #  res_array = quad.qag_array(f,a,b,epsabs,epsrel,6,10000,4)
+    #  end = time.time()
+    #  t1 += end - start
+    #  print(t1)
 
     start = time.time()
-    res_vec = quad.qag_vec(f,a,b,epsabs,epsrel,6,10000)
+    res_vec = quad.qag_vec(f, a, b, epsabs, epsrel, 6, 10000)
     end = time.time()
     t2 += end - start
     print(t2)
@@ -40,29 +38,29 @@ for a in var:
         print(res_vec)
 
 
-print(t1/max)
-print(t2/max)
+print(t1 / max)
+print(t2 / max)
 
 
-    #f = lambda x : np.array([math.cos(1/(x*x)),math.sin(1/(x*x))])
+# f = lambda x : np.array([math.cos(1/(x*x)),math.sin(1/(x*x))])
 #
-    #start = time.time()
-    #y , err = quad_vec(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)
-    #end = time.time()
-    #print(end - start)
-    #print(y)
-    #print(err)
+# start = time.time()
+# y , err = quad_vec(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)
+# end = time.time()
+# print(end - start)
+# print(y)
+# print(err)
 
-    #f = lambda x : math.cos(x)
-    #start = time.time()
-    #y , err = q(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)
-    #f = lambda x : math.sin(x)
-    #y , err = q(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)
-    #end = time.time()
-    #print(end - start)
-    #print(y)
-    #print(err)
+# f = lambda x : math.cos(x)
+# start = time.time()
+# y , err = q(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)
+# f = lambda x : math.sin(x)
+# y , err = q(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)
+# end = time.time()
+# print(end - start)
+# print(y)
+# print(err)
 
 
-#cProfile.run('quad.qag(f,a,b,epsabs,epsrel,6,8)','restats')
-#cProfile.run("quad_vec(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)",'restats2')
+# cProfile.run('quad.qag(f,a,b,epsabs,epsrel,6,8)','restats')
+# cProfile.run("quad_vec(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)",'restats2')
