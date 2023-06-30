a4546a8b0428d8c41f966b9527b9f5deabc7a04c
AlexRaiteri <alex.raiteri@studenti.unimi.it>
2023-06-30 21:37:03 +0200

    Reorganise benches

--- quad-py/benches/bench_scipy.py
+++ quad-py/benches/bench_scipy.py
@@ -1,67 +1,102 @@
-#!/usr/bin/env python3
 import quad
 import math
 import time
-import cProfile
+import timeit
+
 import numpy as np
 from scipy.integrate import quad_vec
 from scipy.integrate import quad as q
 
+def f1(x,delay) :
+    time.sleep(delay)
+    return math.cos(x)
 
-def bench_scipy():
-    max = 1
-    var = range(max)
-
-    t1 = 0.0
-
-    t2 = 0.0
-
-    for k in var:
-        f = lambda x: (math.cos(x), math.sin(x), math.cos(x), math.sin(x))
-        epsabs = 1.0e-3
-        epsrel = 0.0
-        a = 0.0
-        b = 10000.0
-
-        start = time.time()
-        res_array = quad.qag(f, a, b, epsabs, epsrel, 6, 10000, 4)
-        end = time.time()
-        t1 += end - start
-        print(t1)
-
-        start = time.time()
-        res_vec = quad.qag_par(f, a, b, epsabs, epsrel, 6, 10000)
-        end = time.time()
-        t2 += end - start
-        print(t2)
-
-        if a > max - 10:
-            print(res_array)
-            print(res_vec)
-
-    print(t1 / max)
-    print(t2 / max)
+def f2(x,delay) :
+    time.sleep(delay)
+    return math.sin(x)
 
 
-# f = lambda x : np.array([math.cos(1/(x*x)),math.sin(1/(x*x))])
-#
-# start = time.time()
-# y , err = quad_vec(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)
-# end = time.time()
-# print(end - start)
-# print(y)
-# print(err)
+def test_vs_scipy_quad_vec():
+    number = 10
+    a = 0.0
+    b = 1000.0
+    limit = 1000000
+    delay = 1.0e-9
+    #key is used only in quad.qag_vec not in quad_vec
+    key = 2
 
-# f = lambda x : math.cos(x)
-# start = time.time()
-# y , err = q(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)
-# f = lambda x : math.sin(x)
-# y , err = q(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)
-# end = time.time()
-# print(end - start)
-# print(y)
-# print(err)
+    f = lambda x: (
+        f1(x,delay),
+        f2(x,delay),
+        f1(x,delay),
+        f1(x,delay),
+        f2(x,delay),
+        f1(x,delay),
+    )
+
+    g = lambda x : np.array([f1(x,delay),
+                             f2(x,delay),
+                             f1(x,delay),
+                             f1(x,delay),
+                             f2(x,delay),
+                             f1(x,delay),])
 
 
-# cProfile.run('quad.qag(f,a,b,epsabs,epsrel,6,8)','restats')
-# cProfile.run("quad_vec(f,a,b,epsabs=epsabs,epsrel=epsrel,limit=1000000)",'restats2')
+    def bench(test_call,int):
+        times = timeit.repeat(test_call, number=number)
+        print(times)
+        res = test_call()
+        if int == 0 :
+            return min(times), np.array(res.result), res.abserr
+        if int == 1 :
+            return min(times), np.array(res[0]), res[1]
+
+    quad_time, quad_res, quad_err = bench(lambda: quad.qag_vec(f, a, b, limit=limit, key = key), 0)
+    scipy_time, scipy_res, scipy_err = bench(lambda: quad_vec(g, a, b, limit=limit), 1)
+
+    print(f"\nratio: {scipy_time / quad_time * 100:.2f}%")
+    print("distances:", (scipy_res - quad_res) / (scipy_err + quad_err))
+
+def test_vs_scipy_quad():
+    number = 1
+    a = 0.0
+    b = 100.0
+    limit = 1000000
+    delay = 1.0e-7
+    #key is used only in quad.qag_vec not in q
+    key = 2
+
+    f = lambda x: (
+        f1(x,delay),
+        f2(x,delay),
+        f1(x,delay),
+        f1(x,delay),
+        f2(x,delay),
+        f1(x,delay),
+    )
+
+    g1 = lambda x: (
+        f1(x,delay)
+    )
+
+    g2 = lambda x: (
+        f2(x,delay)
+    )
+    def bench(test_call,int):
+        times = timeit.repeat(test_call, number=number)
+        print(times)
+        res = test_call()
+        if int == 0 :
+            return min(times), np.array(res.result), res.abserr
+        if int == 1 :
+            return min(times), np.array([res[0][0],res[1][0],res[2][0],res[3][0],res[4][0],res[5][0]]), math.sqrt(res[0][1]*res[0][1] + res[1][1]*res[1][1])
+
+    quad_time, quad_res, quad_err = bench(lambda: quad.qag_vec(f, a, b, limit=limit, key=key), 0)
+    scipy_time, scipy_res, scipy_err = bench(
+        lambda: (q(g1,a,b,limit=limit),q(g2,a,b,limit=limit),q(g1,a,b,limit=limit),q(g1,a,b,limit=limit),
+                 q(g2,a,b,limit=limit),q(g1,a,b,limit=limit)), 1)
+
+    print(f"\nratio: {scipy_time / quad_time * 100:.2f}%")
+    print("distances:", (scipy_res - quad_res) / (scipy_err + quad_err))
+
+test_vs_scipy_quad()
