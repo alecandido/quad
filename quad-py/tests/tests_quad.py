319a666b4010acfe88758d8aa673d2d70ed40982
AlexRaiteri <alex.raiteri@studenti.unimi.it>
2023-07-04 21:53:16 +0200

    Modified quad-py error test. (is yet to be modified again)

--- quad-py/tests/tests_quad.py
+++ quad-py/tests/tests_quad.py
@@ -1,19 +1,23 @@
 import unittest
 import quad
 import math
+import pytest
 
 
 class MyTestCase(unittest.TestCase):
     def test_max_iteration1(self):
         a = 0.0
         b = 1000.0
-        limit = 100000000
+        limit = 1
         key = 2
         epsabs = 1.0
         epsrel = 1.0
         f = lambda x: (math.cos(x), math.sin(x),)
-        print(quad.qag_vec(f, a, b, epsabs, epsrel,key,limit))
-        self.assertRaises(Exception, quad.qag_vec, (f, a, b, epsabs, epsrel,key,limit))
+        with pytest.raises(TypeError) as exc_info:
+            quad.qag_vec(f, a, b, epsabs, epsrel,key,limit)
+
+        exception_raised = exc_info.value
+        print(exception_raised)
 
 
 if __name__ == '__main__':
