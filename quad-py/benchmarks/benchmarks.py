d48cc920f385dcb8f2667fb7e033177e8f410d47
AlexRaiteri <alex.raiteri@studenti.unimi.it>
2023-07-12 16:36:51 +0200

    Try to add param to benchmarks

--- quad-py/benchmarks/benchmarks.py
+++ quad-py/benchmarks/benchmarks.py
@@ -16,7 +16,7 @@ def f2(x, delay):
     return math.sin(x)
 
 
-def f3(x, delay):
+def f_1(x, delay):
     return (
         f1(x, delay),
         f2(x, delay),
@@ -26,6 +26,11 @@ def f3(x, delay):
         f1(x, delay),
     )
 
+def f_2(x, delay):
+    return (
+        f1(x, delay),
+        f2(x, delay),
+    )
 
 def f_scipy(x, delay):
     return np.array(
@@ -40,30 +45,34 @@ def f_scipy(x, delay):
     )
 
 
-class QagVecVsQagPar:
+class QagBench:
     timeout = 10000.0
     warmup_time = 3.0
     repeat = (1, 10, 1200.0)
     params = ([0.0, 1.0e-7, 1.0e-4], [100.0, 1000.0, 10000.0])
     param_names = ["delay", "b"]
 
+
     def setup(self, delay, b):
         self.a = 0.0
         self.limit = 1000000
         self.key = 2
-        self.f = lambda x: f3(x, delay)
+        self.f = lambda x: f_1(x, delay)
         self.f_scipy = lambda x: f_scipy(x, delay)
         self.f1_scipy = lambda x: f1(x, delay)
         self.f2_scipy = lambda x: f2(x, delay)
 
-    def time_qag_par(self, delay, b):
+    def time_qag_par(self, delay, b, fun):
+        f = lambda x: fun(x, delay)
         quad.qag(
-            self.f,
+            f,
             self.a,
             b,
             limit=self.limit,
             key=self.key,
         )
+    time_qag_par.params = [f_1,f_2]
+    time_qag_par.param_names = ["fun"]
 
     def time_scipy_vec(self, delay, b):
         quad_vec(self.f_scipy, self.a, b, limit=self.limit)
