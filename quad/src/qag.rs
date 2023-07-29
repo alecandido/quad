34d16088d6faaed957aa2d3cecefd9321192e591
AlexRaiteri <alex.raiteri@studenti.unimi.it>
2023-07-29 18:33:54 +0200

    Start documenting qag.rs .

--- quad/src/qag.rs
+++ quad/src/qag.rs
@@ -45,108 +45,16 @@ pub struct Qag {
     pub more_info: bool,
 }
 
-///           f      : f64
-///                     function
-///
-///           a      : f64
-///                    lower limit of integration
-///
-///           b      : f64
-///                    upper limit of integration
-///
-///           epsabs : f64
-///                    absolute accuracy requested
-///
-///           epsrel : f64
-///                    relative accuracy requested
-///                    if  epsabs <= 0 && epsrel <= max(50*rel.mach.acc.,0.5d-28),
-///                    the fn will return with result_state = Invalid.
-///
-///            key   : i32
-///                    key for choice of local integration rule. A gauss-kronrod pair is used with:
-///                          7 - 15 points if key < 2,
-///                         10 - 21 points if key = 2,
-///                         15 - 31 points if key = 3,
-///                         20 - 41 points if key = 4,
-///                         25 - 51 points if key = 5,
-///                         30 - 61 points if key > 5.
-///
-///            limit : i32
-///                    gives an upperbound on the number of subintervals in the partition
-///                    of (a,b), limit >= 1.
-///
-///
-///
-///         On return : QagIntegratorResult :
-///
-///           QagIntegrationResult:
-///           result : f64
-///                    Approximation to the integral.
-///
-///           abserr : f64
-///                    Estimate of the modulus of the absolute error,
-///                    which should equal or exceed abs(i-result).
-///
-///           neval  : i32
-///                    Number of integrand evaluations.
-///
-///           alist  : Vec<f64>
-///                      Vector of dimension at least limit, the elements of which are the left
-///                      end points of the subintervals in the partition of the given integration
-///                      range (a,b).
-///
-///           blist  : Vec<f64>
-///                      Vector of dimension at least limit, the elements of which are the right
-///                      end points of the subintervals in the partition of the given integration
-///                      range (a,b).
-///
-///           rlist  : Vec<f64>
-///                      Vector of dimension at least limit, the elements of which are the integral
-///                      approximations on the subintervals.
-///
-///            rlist  : Vec<f64>
-///                      Vector of dimension at least limit, the elements of which are the moduli
-///                      of the absolute error estimates on the subintervals.
-///
-///            iord   : Vec<usize>
-///                      Vector of dimension at least limit, the elements of which are pointers to
-///                      the error estimates over the subintervals, such that
-///                      elist(iord(1)), ...,elist(iord(k)) form a decreasing sequence,
-///                      with k = last if last <= (limit/2+2), and
-///                      k = limit+1-last otherwise.
-///
-///            last    : usize
-///                      number of subintervals actually produced in the
-///                      subdivision process
-///
-///
-///
-///
-///           ResultState =
-///           Success :
-///                    Normal and reliable termination of the routine. it is assumed that the
-///                    requested accuracy has been achieved.
-///           MaxIteration :
-///                    The maximum number of steps has been executed. the integral is probably too
-///                    difficult to be calculated by dqng.
-///           Invalid :
-///                     The input is invalid, because epsabs <= 0 &&
-///                     epsrel < max(50 * rel.mach.acc.,0.5e-28).
-///           BadTolerance :
-///                     The occurrence of roundoff error is detected, which prevents the requested
-///                     tolerance from being achieved.
-///           BadFunction :
-///                     Extremely bad integrand behaviour occurs at some points of the integration
-///                     interval.
-///
-///
-///           If ResultState != Succes =>    It is assumed that the requested accuracy has not
-///           been achieved.
-///
-///
-///
-
 impl Qag {
+    /// Adaptive integration of a vector-valued function.
+    ///
+    /// If the interval is finite, [qintegrate](Qag::qintegrate) is called.
+    ///
+    /// If the interval is semi-infinite or infinite, the function is transformed using
+    /// [semi_infinite_function] or [double_infinite_function], also the optional points, if
+    /// present, are transformed using [points_transformed]. After that [qintegrate](Qag::qintegrate)
+    /// is called using (0,1) or (1,-1) as new interval for the semi-infinite and infinite case
+    /// respectively.
     pub fn integrate(
         &self,
         fun: &FnVec,
@@ -190,6 +98,10 @@ impl Qag {
         self.qintegrate(&fun, a, b, epsabs, epsrel)
     }
 
+    /// Adaptive integration of a vector-valued function.
+    ///
+    /// This function is not intended to be called directly.
+    /// Use [integrate](Qag::integrate) instead.
     pub fn qintegrate(
         &self,
         fun: &FnVec,
